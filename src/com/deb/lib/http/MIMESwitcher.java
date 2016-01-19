b61b6f4a174eb0e6b3336bd36108ab81596a8dde
David Boles <david.e.boles22@gmail.com>
2016-01-18 18:18:45 -0800

    Added addition MIME support

--- src/com/deb/lib/http/MIMESwitcher.java
+++ src/com/deb/lib/http/MIMESwitcher.java
@@ -25,7 +25,28 @@ public class MIMESwitcher {
 	}
 
 	public static final String[][] types =  {
-		{".png", "images/png"}
+		{".png", "images/png"},
+		{".bmp", "image/bmp"},
+		{".gif", "image/gif"},
+		{".ico", "image/x-icon"},
+		{".jpg", "image/jpeg"},
+		{".jpeg", "image/jpeg"},
+		{".svg", "image/svg+xml"},
+		{".tiff", "image/tiff"},
+		{".css", "text/css"},
+		{".csv", "text/csv"},
+		{".html", "text/html"},
+		{".rtf", "text/richtext"},
+		{".tsv", "text/tab-separated-values"},
+		{".txt", "text/plain"},
+		{".avi", "video/x-msvideo"},
+		{".f4v", "video/x-f4v"},
+		{".flv", "video/x-fl"},
+		{".h261", "video/h261"},
+		{".h263", "video/h263"},
+		{".h264", "video/h264"},
+		{".mpeg", "video/mpeg"},
+		{".mp4", "video/mp4"},
 		//TODO add types
 	};
 }
